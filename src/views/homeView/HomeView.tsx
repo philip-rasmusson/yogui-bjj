642867920d71ba119935dd7b75c901578fcaa3be
Zia Alekozai <88ziaale@gafe.molndal.se>
2021-06-13 11:29:39 +0200

    gjorde klart funktionen för homeVyn

--- src/views/homeView/HomeView.tsx
+++ src/views/homeView/HomeView.tsx
@@ -1,15 +1,20 @@
 import './HomeView.css'
+import { useHistory } from 'react-router-dom'
+import RoutingPath from '../../routes/RoutingPath'
+
 export const HomeView = () => {
+
+    const history = useHistory()
     return (
         <div className='HomeViewWrapper'>
-            <button className='buttonOne'>LOGGA IN</button>
+            <button className='buttonOne'onClick={() => history.push(RoutingPath.signView)}>LOGGA IN</button>
 
             <div className='CreateAccount'>
-                <span>INTE MEDLEM? SKAPA GRATIS KONTO HÄR</span></div>
+                <span onClick={() => history.push(RoutingPath.registrationView)}>INTE MEDLEM? SKAPA GRATIS KONTO HÄR</span></div>
 
-            <br /><button className='buttonTwo'>VÄLJ TRÄNINGSFORM</button>
+            <br /><button className='buttonTwo'onClick={() => history.push(RoutingPath.trainingForm)}>VÄLJ TRÄNINGSFORM</button>
 
-            <button className='buttonThree'>FAQ</button>
+            <button className='buttonThree'onClick={() => history.push(RoutingPath.fAQ)}>FAQ</button>
         </div>
     )
 }
