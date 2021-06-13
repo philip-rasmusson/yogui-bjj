2ad0f3046c8bc0508694c3193b2806644887cc1c
Zia Alekozai <88ziaale@gafe.molndal.se>
2021-06-13 10:50:40 +0200

    Skapade HomeView

--- src/views/homeView/HomeView.tsx
+++ src/views/homeView/HomeView.tsx
@@ -1,8 +1,15 @@
-
+import './HomeView.css'
 export const HomeView = () => {
     return (
-        <div>
-           
+        <div className='HomeViewWrapper'>
+            <button className='buttonOne'>LOGGA IN</button>
+
+            <div className='CreateAccount'>
+                <span>INTE MEDLEM? SKAPA GRATIS KONTO HÄR</span></div>
+
+            <br /><button className='buttonTwo'>VÄLJ TRÄNINGSFORM</button>
+
+            <button className='buttonThree'>FAQ</button>
         </div>
     )
 }
