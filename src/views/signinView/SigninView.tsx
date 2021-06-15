68794ff04e2e03e26ac51085ad67a00727e1f82b
Philip Rasmusson <studiobigmouth@gmail.com>
2021-06-15 16:23:11 +0200

    added design to sign in view

--- src/views/signinView/SigninView.tsx
+++ src/views/signinView/SigninView.tsx
@@ -4,13 +4,13 @@ export const SigninView = () => {
   return (
     <div>
       <form action="" className="signin-form">
-        <h1 className="signin-title">Sign in</h1>
-        <input type="text" className="signin-username" id="username" />
-        <input type="password" className="signin-password" id="password" />
-        <button type="submit" className="signin-button">Sign in</button>
+        <h1 className="signin-title">Logga in</h1>
+        <input type="text" className="signin-username" id="username" placeholder="Användarnamn" />
+        <input type="password" className="signin-password" id="password" placeholder="Lösenord" />
+        <button type="submit" className="signin-button">Logga in</button>
         <span className="stay-logged-in">
           <input type="checkbox" className="signin-checkbox" id="stay-logged-in" />
-          <p>Stay logged in</p>
+          <p>Fortsätt vara inloggad</p>
         </span>
       </form>
 
