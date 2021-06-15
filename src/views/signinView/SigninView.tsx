9e8a3cd8583908bf1e011a5175a1f732d661bff3
Philip Rasmusson <studiobigmouth@gmail.com>
2021-06-15 21:22:17 +0200

    Added links in navigation, box-shadow as Global className, removed obsolete RoutingPaths, minor changes to HomeView

--- src/views/signinView/SigninView.tsx
+++ src/views/signinView/SigninView.tsx
@@ -7,7 +7,7 @@ export const SigninView = () => {
         <h1 className="signin-title">Logga in</h1>
         <input type="text" className="signin-username" id="username" placeholder="Användarnamn" />
         <input type="password" className="signin-password" id="password" placeholder="Lösenord" />
-        <button type="submit" className="signin-button">Logga in</button>
+        <button type="submit" className="signin-button box-shadow-button">Logga in</button>
         <span className="stay-logged-in">
           <input type="checkbox" className="signin-checkbox" id="stay-logged-in" />
           <p>Fortsätt vara inloggad</p>
