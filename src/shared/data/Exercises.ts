e502aed16320dcc5431eb739afb11654f03c11d6
Philip Rasmusson <studiobigmouth@gmail.com>
2021-06-23 08:51:58 +0200

    spell checked

--- src/shared/data/Exercises.ts
+++ src/shared/data/Exercises.ts
@@ -3,9 +3,9 @@ const dumbbellFly = {
 	steps: [
 		'Sätt dig på en platt eller lätt lutande bänk med fötterna mot golvet och vikterna vilandes mot benen ovanför dina knän.',
 		'Luta dig bakåt tills huvud och rygg vilar mot bänken och för samtidigt ihop vikterna och sträck ut din armar.',
-		'Vikterna är nu tryckta ihop ovanför ditt bröst och dina armar utsträckta, det här är start positionen.',
+		'Vikterna är nu tryckta ihop ovanför ditt bröst och dina armar utsträckta, det här är startpositionen.',
 		'Sänk sakta vikterna mot golvet på vardera sida om dig - med armar fortsatt utsträckta - fokusera på att kontrollera sänkningen med musklerna i bröst och axlar ',
-		'Höj vikterna tills de når start position ovanför ditt bröst igen, och fokusera på att kontrollera höjningen med musklerna i bröst och axlar.',
+		'Höj vikterna tills de når startposition ovanför ditt bröst igen, och fokusera på att kontrollera höjningen med musklerna i bröst och axlar.',
 	],
 	musclegroups: ['pectoralis major', 'deltoideus anterior', 'triceps brachii'],
 	author: 'Karlus Sjöstrandius',
