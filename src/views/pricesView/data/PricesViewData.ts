be3d1fac3dca1f01fa04472fe7629273199274bf
Philip Rasmusson <studiobigmouth@gmail.com>
2021-08-22 21:20:34 +0200

    moved PricesWrapper to external file

--- src/views/pricesView/data/PricesViewData.ts
+++ src/views/pricesView/data/PricesViewData.ts
@@ -1,22 +1,45 @@
 const title = 'priser & betalning'
 const bgImg = 'prices'
 
-const pricesTitle1 = 'barn'
-const pricesSubTitle1 = ' (upp til 9 år)'
+const pricesTitleKids = 'bjj barn'
+const pricesSubTitleKids = ' (upp til 9 år)'
 const pricesKids1 = '1995 kr / termin'
 const pricesKids1info = '2 ggr / vecka'
 const pricesKids2 = '2145 kr / termin'
 const pricesKids2info = '4 ggr / vecka'
 
+const pricesTitleYouth = 'bjj ungdomar'
+const pricesYouth1 = '2250 kr / termin'
+const pricesYouth1info = '2 ggr / vecka'
+
+const pricesTitleBJJ = 'BJJ Vuxen'
+const pricesBJJ1 = '2450 kr / termin'
+
+const pricesTitleBJJandSW = 'BJJ & Submission Wrestling'
+const pricesBJJandSW1 = '2995 kr / termin'
+
+const pricesTitleSW = 'Submission Wrestling'
+const pricesSW1 = '1995 kr / termin'
+
 const PricesViewData = {
   title,
   bgImg,
-  pricesTitle1,
-  pricesSubTitle1,
+  pricesTitleKids,
+  pricesSubTitleKids,
   pricesKids1,
   pricesKids1info,
   pricesKids2,
-  pricesKids2info
+  pricesKids2info,
+  pricesTitleYouth,
+  pricesYouth1,
+  pricesYouth1info,
+  pricesTitleBJJ,
+  pricesBJJ1,
+  pricesTitleBJJandSW,
+  pricesBJJandSW1,
+  pricesTitleSW,
+  pricesSW1
+
 }
 
 export default PricesViewData
--- src/views/pricesView/pricesViewMobile/PricesViewMobile.tsx
+++ src/views/pricesView/pricesViewMobile/PricesViewMobile.tsx
@@ -1,32 +1,48 @@
 import { DefaultHeaderMobile } from '../../../shared/components/defaultHeader/defaultHeaderMobile/DefaultHeaderMobile'
 import './PricesViewMobile.css'
 import PricesViewData from '../data/PricesViewData'
-import { PricesView } from '../PricesView'
+import { PricesWrapper } from '../components/PricesWrapper'
 
 export const PricesViewMobile = () => {
-
-  const pricesWrapper = (priceTitle: string, price1: string, price1info: string, price2?: string, price2info?: string, priceSubTitle?: string) => {
-    return (
-      <div className="prices-view-mobile-prices-wrapper">
-        <h3>{priceTitle}<span>{priceSubTitle}</span></h3>
-        <div>
-          <p className="col-1 row-1">{price1}</p>
-          <p className="col-2 row-1">{price1info}</p>
-          <p className="col-1 row-2">{price2}</p>
-          <p className="col-2 row-2">{price2info}</p>
-        </div>
-      </div>
-
-    )
-  }
 
   return (
     <div className="prices-view-mobile-wrapper">
       <DefaultHeaderMobile title={PricesViewData.title} bgImg={PricesViewData.bgImg} />
+
       {/* BBJ KIDS */}
-      {pricesWrapper(PricesViewData.pricesTitle1, PricesViewData.pricesKids1, PricesViewData.pricesKids1info, PricesViewData.pricesKids2, PricesViewData.pricesKids2info, PricesViewData.pricesSubTitle1)}
+      <PricesWrapper
+        title={PricesViewData.pricesTitleKids}
+        subTitle={PricesViewData.pricesSubTitleKids}
+        price1={PricesViewData.pricesKids1}
+        price1info={PricesViewData.pricesKids1info}
+        price2={PricesViewData.pricesKids2}
+        price2info={PricesViewData.pricesKids2info}
+      />
+
       {/* BJJ YOUTH */}
-      {pricesWrapper(PricesViewData.pricesTitle1, PricesViewData.pricesKids1, PricesViewData.pricesKids1info)}
+      <PricesWrapper
+        title={PricesViewData.pricesTitleYouth}
+        price1={PricesViewData.pricesYouth1}
+        price1info={PricesViewData.pricesYouth1info}
+      />
+
+      {/* BJJ adult */}
+      <PricesWrapper
+        title={PricesViewData.pricesTitleBJJ}
+        price1={PricesViewData.pricesBJJ1}
+      />
+
+      {/* BJJ+SW adult */}
+      <PricesWrapper
+        title={PricesViewData.pricesTitleBJJandSW}
+        price1={PricesViewData.pricesBJJandSW1}
+      />
+
+      {/* SW adult */}
+      <PricesWrapper
+        title={PricesViewData.pricesTitleSW}
+        price1={PricesViewData.pricesSW1}
+      />
 
     </div>
   )
