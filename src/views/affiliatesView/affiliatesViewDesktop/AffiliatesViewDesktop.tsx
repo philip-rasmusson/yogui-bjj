c578029c8fc70b5d93a9204f69a1383c8b703c55
Philip Rasmusson <studiobigmouth@gmail.com>
2021-09-18 15:13:12 +0200

    work on affiliate card and desktop

--- src/views/affiliatesView/affiliatesViewDesktop/AffiliatesViewDesktop.tsx
+++ src/views/affiliatesView/affiliatesViewDesktop/AffiliatesViewDesktop.tsx
@@ -2,19 +2,51 @@ import { DefaultSectionOneDesktop } from '../../../shared/components/defaultSect
 import { AffiliateCard } from '../components/AffiliateCard'
 import './AffiliatesViewDesktop.css'
 import marcelo from '../../../shared/img/homeImgs/sq/marcelo-sq.jpg'
+import main from '../../../shared/img/affiliates/torslanda/main.png'
+import x from '../../../shared/img/instructors/marcelo-sq.jpg'
 
 export const AffiliatesViewDesktop = () => {
+
+  const instructorsTorslanda = [
+    { src: x },
+    { src: x },
+    { src: x },
+    { src: x },
+    { src: x },
+  ]
+
   return (
     <div className="affiliates-view-desktop-wrapper">
-      <AffiliateCard
-        title="torslanda kampsportcenter"
-        titleEng=""
-        text="hangarvägen"
-        textEng=""
-        btnText="besök hemsida"
-        btnTextEng=""
-        img={marcelo}
-      />
+      <div className="affiliates-view-desktop-default font-grey">
+        <AffiliateCard
+          title="torslanda kampsportcenter"
+          adress="Hangarvägen 5, 342 56  TORSLANDA, SVERIGE"
+          btnText="besök hemsida"
+          btnTextEng=""
+          img={main}
+          instructors={instructorsTorslanda}
+        />
+      </div>
+      <div className="affiliates-view-desktop-color font-white">
+        <AffiliateCard
+          title="torslanda kampsportcenter"
+          adress="hangarvägen"
+          btnText="besök hemsida"
+          btnTextEng=""
+          img={main}
+          instructors={instructorsTorslanda}
+        />
+      </div>
+      <div className="affiliates-view-desktop-default font-grey">
+        <AffiliateCard
+          title="torslanda kampsportcenter"
+          adress="hangarvägen"
+          btnText="besök hemsida"
+          btnTextEng=""
+          img={main}
+          instructors={instructorsTorslanda}
+        />
+      </div>
     </div>
   )
 }
--- src/views/affiliatesView/components/AffiliateCard.tsx
+++ src/views/affiliatesView/components/AffiliateCard.tsx
@@ -7,35 +7,43 @@ import { useContext } from 'react'
 import { LanguageContext } from '../../../shared/provider/LanguageProvider'
 
 
-export const AffiliateCard = (props: { img: any, title: string, titleEng: string, text: string, textEng: string, btnText: string, btnTextEng: string, btnLink?: string }) => {
+export const AffiliateCard = (props: { img: any, title: string, adress?: string, email?: string; homepage?: string, btnText: string, btnTextEng: string, btnLink?: string, instructors?: Array<any> }) => {
   const [language] = useContext(LanguageContext)
 
+  // const displayInstructors = () => {
+  //   return <img src={props.instructors} />
+  // }
+  const displayInstructors = props.instructors?.map((imgSrc, i) => {
+    return <img src={imgSrc.src} key={i} alt={imgSrc.src} />
+
+  })
 
   useEffect(() => {
   }, [language])
 
-
   return (
-    <div className='affiliate-card-wrapper font-black'>
-      <h2>
-        <SelectLanguage
-          textSwe={props.title}
-          textEng={props.titleEng}
-        />
-      </h2>
+    <div className='affiliate-card-wrapper'>
       <div className="affiliate-card-img-wrapper">
         <img src={props.img} alt={props.title} className="box-shadow" />
-      </div>
-      <div className="affiliate-card-text-wrapper">
-
-        <p><SelectLanguage
-          textSwe={props.text}
-          textEng={props.textEng}
-        /></p>
-        <button className="bg-color-black font-white box-shadow"><SelectLanguage
+        {/* <button className="bg-color-black font-white box-shadow"><SelectLanguage
           textSwe={props.btnText}
           textEng={props.btnTextEng}
-        /></button>
+        /></button> */}
+      </div>
+      <div className="affiliate-card-text-wrapper">
+        <h2>{props.title}
+        </h2>
+        <div className='affiliate-card-text-info'>
+          <p className="col-1">Adress:</p>
+          <p className="col-2">{props.adress}</p>
+          <p className="col-1">Email:</p>
+          <p className="col-2">{props.email}</p>
+          <p className="col-1">Hemsida:</p>
+          <p className="col-2">{props.homepage}</p>
+        </div>
+        <div className="affiliate-card-instructors-wrapper">
+          {displayInstructors}
+        </div>
       </div>
     </div>
   )
