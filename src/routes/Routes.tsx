42bc66fb774bd04dd2f1b36acf3c16d4492c41b7
Philip Rasmusson <philiprasmusson@philips-mbp.mynet>
2021-10-17 18:35:42 +0200

    deployed live

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import { Footer } from '../components/footer/Footer'
 import { HomeView } from '../views/homeView//HomeView'
 import { TechniquesView } from '../views/techniquesView/TechniquesView'
